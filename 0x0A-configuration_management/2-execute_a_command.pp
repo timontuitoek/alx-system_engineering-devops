9ecdd65c22e166ef6f26d29db4cd681615aeb5a1
timontuitoek <tuitoektimon.com>
2023-11-25 00:24:05 +0300

    A manifest that kills a process named killmenow

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,6 @@
 # Kill the process named "killmenow" using pkill
 
-exec { 'killmenow_process':
-  command     => 'pkill -f killmenow',
-  refreshonly => true,
-  onlyif      => 'pgrep -f killmenow',
+exec { 'kill':
+  command     => 'pkill killmenow',
+  path    => '/usr/local/bin/:/bin/'
 }
