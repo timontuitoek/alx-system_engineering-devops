5b0745f46896466b9b2468de081dd024f9d94682
timontuitoek <tuitoektimon.com>
2023-11-26 23:37:20 +0300

    Using Puppet to make changes to our configuration file

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+exec { 'Append Line IdentityFile':
+  command => '/bin/echo -e "IdentityFile ~/.ssh/school\nPasswordAuthentication no" >> /etc/ssh/ssh_config',
+  path    => '/etc/ssh/ssh_config',
+}
