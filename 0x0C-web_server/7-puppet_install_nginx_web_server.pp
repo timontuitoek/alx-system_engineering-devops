5bcc1e13d9b2a6c2e5ded4458cc678e488d18f8a
timontuitoek <tuitoektimon.com>
2023-11-29 01:45:01 +0300

    Install Nginx web server

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,10 @@
+# Script to install nginx using puppet
+
+package {'nginx':
+  ensure => 'present',
+}
+
+exec {'install':
+  provider => shell,
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/fabrizia2 permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
+}
