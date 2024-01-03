d57f37b52f3c2614a639347ecc60af06d262ee5c
timontuitoek <tuitoektimon@gmail.com>
2024-01-03 10:07:24 +0300

    update

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,46 +7,25 @@ import requests
 import sys
 
 
-def get_employee_todo_progress(employee_id):
-    """
-    Get employee TODO list progress and display on standard output.
-
-    Args:
-        employee_id (int): The ID of the employee.
-    """
-    base_url = "https://jsonplaceholder.typicode.com"
-    user_url = f"{base_url}/users/{employee_id}"
-    todos_url = f"{base_url}/todos?userId={employee_id}"
-
-    # Fetch user information
-    user_response = requests.get(user_url)
-    user_data = user_response.json()
-    employee_name = user_data.get("name")
-
-    # Fetch todos for the user
-    todos_response = requests.get(todos_url)
-    todos_data = todos_response.json()
-
-    # Calculate progress
-    total_tasks = len(todos_data)
-    completed_tasks = sum(1 for todo in todos_data if todo.get("completed"))
-
-    # Display progress information
-    print(f"Employee {employee_name} is done with tasks /"
-          f"({completed_tasks}/{total_tasks}): ")
-    print(f"{employee_name}: {completed_tasks}/{total_tasks}")
-
-    # Display completed tasks titles
-    for todo in todos_data:
-        if todo.get("completed"):
-            print(f"\t{todo['title']}")
-
-
 if __name__ == "__main__":
+    """
+    crawls the API and returns the employee progress
+    """
+    user_id = sys.argv[1]
+    base_url = 'https://jsonplaceholder.typicode.com/'
+    user_str = '{}users/{}'.format(base_url, user_id)
+    todos_str = '{}todos?userId={}'.format(base_url, user_id)
+    employee_str = "Employee {} is done with tasks"
 
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <employee_id>")
-        sys.exit(1)
+    res = requests.get(user_str)
+    print(employee_str.format(res.json().get('name')), end="")
 
-    employee_id = int(sys.argv[1])
-    get_employee_todo_progress(employee_id)
+    res = requests.get(todos_str)
+    tasks = []
+    for task in res.json():
+        if task.get('completed') is True:
+            tasks.append(task)
+
+    print("({}/{}):".format(len(tasks), len(res.json())))
+    for task in tasks:
+        print("\t {}".format(task.get("title")))
--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -11,24 +11,23 @@ import csv
 
 
 if __name__ == "__main__":
-    """
-    crawls the API and returns the employee progress
-    """
     user_id = sys.argv[1]
     base_url = 'https://jsonplaceholder.typicode.com/'
     user_str = '{}users/{}'.format(base_url, user_id)
     todos_str = '{}todos?userId={}'.format(base_url, user_id)
-    employee_str = "Employee {} is done with tasks"
+    file = '{}.csv'.format(user_id)
 
     res = requests.get(user_str)
-    print(employee_str.format(res.json().get('name')), end="")
+    username = res.json().get('username')
 
     res = requests.get(todos_str)
     tasks = []
     for task in res.json():
-        if task.get('completed') is True:
-            tasks.append(task)
+        tasks.append([user_id, username,
+                      task.get('completed'), task.get('title')])
 
-    print("({}/{}):".format(len(tasks), len(res.json())))
-    for task in tasks:
-        print("\t {}".format(task.get("title")))
+    with open(file, mode='w') as emp_file:
+        emp_writer = csv.writer(emp_file, delimiter=',', quotechar='"',
+                                quoting=csv.QUOTE_ALL)
+        for task in tasks:
+            emp_writer.writerow(task)
