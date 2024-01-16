460ff389e75021881015302cf61a32478b86cea6
timontuitoek <tuitoektimon@gmail.com>
2024-01-16 21:48:37 +0300

    A function that queries the Reddit API and returns the number of subscribers (not active users

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,43 +1,17 @@
 #!/usr/bin/python3
-
-"""
-This script queries the Reddit API.
-"""
-
 import requests
 
 
 def number_of_subscribers(subreddit):
     """
-    Get the number of subscribers for a given subreddit.
+    Queries the Reddit API.
     """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'your_custom_user_agent'}
+    headers = {'User-Agent': 'My User Agent 1.0'}
+    response = requests.get
+    (f"https://www.reddit.com/r/{subreddit}/about.json",
+        headers=headers, allow_redirects=False)
 
-    try:
-        response = requests.get(url, headers=headers)
-
-        # Check if the request was successful (status code 200)
-        if response.status_code == 200:
-            # Parse the JSON response and extract the number of subscribers
-            data = response.json()
-            subscribers_count = data['data']['subscribers']
-            return subscribers_count
-        elif response.status_code == 404:
-            # Subreddit not found, return 0
-            return 0
-        else:
-            # Other error, return 0
-            return 0
-
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    if response.status_code != 200:
         return 0
 
-
-if __name__ == "__main__":
-    # Example usage:
-    subreddit_name = 'python'
-    subscribers_count = number_of_subscribers(subreddit_name)
-    print(f"The number of subscribers in r/{subreddit_name}:
-          {subscribers_count}")
+    return response.json()['data']['subscribers']
